45cec546173d3b7b3a0ed7f4dcd0c644598eb9f9
00157560 <00157560@LTODC370.GLOBAL.MBB.DIR>
2025-08-11 14:50:52 +0530

    my first project

---
+++ mbbafternoon/src/main/java/com/mbbafternoon/App.java
@@ -0,0 +1,13 @@
+package com.mbbafternoon;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
